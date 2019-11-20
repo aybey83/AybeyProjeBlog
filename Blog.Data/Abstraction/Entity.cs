1fb18261ddb56653f0de0f3537afb30e82fc82e1
aybey83 <aybey83@hotmail.com>
2019-11-20 21:21:45 +0300

    20.11.2019 21:21 Ders Sonu :)

---
+++ Blog.Data/Abstraction/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data.Abstraction
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool Deleted { get; set; }
+        
+    }
+}
